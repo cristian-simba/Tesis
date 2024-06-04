c51fd18a784ec3a470d0a54621aeb007c01feb16
cristian-simba <davidsimba200@gmail.com>
2024-06-03 21:56:46 -0500

    confirmar cuenta

--- src/pages/public/ConfirmarCuenta.jsx
+++ src/pages/public/ConfirmarCuenta.jsx
@@ -1,25 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import { FaCheck, FaTimes } from 'react-icons/fa';
+import { Flex, Spinner } from '@radix-ui/themes';
 
-export default function ConfirmarCuenta() {
+const ConfirmarCuenta = () => {
+  const { token } = useParams();
+  const [mensaje, setMensaje] = useState('');
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+
+  useEffect(() => {
+    const confirmarCuenta = async () => {
+      try {
+        const response = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/confirmar/${token}`);
+        setMensaje(response.data.msg);
+        setError(false);
+      } catch (error) {
+        setMensaje(error.response.data.msg);
+        setError(true);
+      } finally {
+        setLoading(false)
+      }
+    };
+
+    confirmarCuenta();
+  }, [token]);
+
   return (
     <>
-      <div className="bg-gray-100 flex items-center justify-center pt-20">
-        <div className="bg-white p-6 rounded-lg shadow-lg flex items-center space-x-4 radix-styled">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6 text-green-500"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            strokeWidth="2"
-          >
-            <path strokeLinecap="round" strokeLinejoin="round" d="M5 13l4 4L19 7" />
-          </svg>
+    {loading ? (
+      <Flex justify='center' align='center' className='min-h-screen'>
+        <Spinner />
+      </Flex>
+      ) :(
+      <div className="flex items-center justify-center pt-20">
+        <div className={`p-6 rounded-lg shadow-lg flex items-center space-x-4 ${error ? 'bg-red-10  0' : 'bg-green-100'}`}>
+          {error ? <FaTimes color='red' /> : <FaCheck color='green' />}
           <div className="text-lg font-semibold text-gray-700">
-            Tu cuenta ha sido activada
+            {mensaje}
           </div>
         </div>
       </div>
+    )}
     </>
   );
-}
+};
+
+export default ConfirmarCuenta;
