6f3feae10f0047e2b87520241b2b14a3c645be9c
cristian-simba <davidsimba200@gmail.com>
2024-08-01 09:13:42 -0500

    Descarga de la app

--- src/components/LandingPage/Buttons/LandingBtns.jsx
+++ src/components/LandingPage/Buttons/LandingBtns.jsx
@@ -3,11 +3,16 @@ import { Button } from "@radix-ui/themes";
 import { useNavigate } from "react-router-dom";
 
 export function DowloadButton({ text }) {
+  const handleClick = () => {
+    window.location.href = 'https://expo.dev/artifacts/eas/dC3MUqU5RNEqLJB62CQ6ra.apk';
+  };
+
   return (
     <Button
       radius="none"
       size={{ md: "3", lg: "4" }}
       className="hover:cursor-pointer"
+      onClick={handleClick}
     >
       {text}
     </Button>
